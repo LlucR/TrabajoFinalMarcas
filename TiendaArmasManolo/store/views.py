ada0357138f7932b41fbd4e790491626a755079a
LlucR <lruiz6@nigul.cide.es>
2023-04-03 18:59:21 +0200

    Mariano Rajoy 1

--- TiendaArmasManolo/store/views.py
+++ TiendaArmasManolo/store/views.py
@@ -5,5 +5,5 @@ def titulo(request):
     return HttpResponse("Bienvenido a Tienda De Armas Manolo") 
 def about(request):
     return HttpResponse("D:\Cosas\TrabajoMarcas\AhoraSiProyectoFinalMarcas\AboutUs\SobreNosotros.html")
-class ModelView(TemplateView):
-    template_name = ".html"
+def AboutUs(request):
+    context = {"../AboutUS/SobreNosotros.html"}
